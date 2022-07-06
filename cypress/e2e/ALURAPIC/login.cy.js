11e30645f1403a81baae32fbb935c31ee83b1235
Rodrigo Augusto <rodrigoaugusto839@gmail.com>
2022-07-06 14:41:26 -0300

    simulando objeto - tipo o mock

--- cypress/e2e/ALURAPIC/login.cy.js
+++ cypress/e2e/ALURAPIC/login.cy.js
@@ -3,11 +3,17 @@ describe('Login de usuarios alura pic ', () => {
     beforeEach(() => {
         
         cy.visit('https://alura-fotos.herokuapp.com')
+
+        // cy.intercept('POST', 'https://apialurapic.herokuapp.com/user/login', { //mockando
+        //     statusCode:400
+        // }).as('stubPost')
     })
 
     it('fazer login de usuario valido', () => {
 
         cy.login(Cypress.env('userName'), Cypress.env('password')) // comando personalizado
+
+        // cy.wait('@stubPost') //usando o mock
 
         cy.contains('a', '(Logout)').should('be.visible');
 
