405dc634d0a8bd8d8558e241a5d4c0e3e03cf670
Rodrigo Augusto <rodrigo.augusto@inatel.br>
2022-07-06 11:23:13 -0300

    comando personalizado

--- cypress/e2e/ALURAPIC/alurapic.cy.js
+++ cypress/e2e/ALURAPIC/alurapic.cy.js
@@ -50,4 +50,31 @@ describe('Login e registro de usuarios alura pic ', () => {
         cy.contains('ap-vmessage', 'Must be lower case').should('be.visible')
 
     })
+
+    it.only('fazer login de usuario valido', () => {
+
+        cy.login('flavio', '123') // comando personalizado
+
+        cy.get('input[formcontrolname="userName"]').type('flavio');
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.get('button[type="submit"]').click();
+        
+        cy.contains('a', '(Logout)').should('be.visible');
+
+    })
+
+    it.only('fazer login de usuario invalido', () => {
+
+        cy.login('jacqueline', '1234') // comando personalizado
+        // cy.get('input[formcontrolname="userName"]').type('jacqueline');
+        // cy.get('input[formcontrolname="password"]').type('1234');
+        // cy.get('button[type="submit"]').click();
+        
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Invalid user name or password')
+        })
+
+    })
+
+
 })
