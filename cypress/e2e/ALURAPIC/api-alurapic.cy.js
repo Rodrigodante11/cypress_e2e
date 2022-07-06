bd316f02f5eedacd75a5cba159901a9633b79224
Rodrigo Augusto <rodrigoaugusto839@gmail.com>
2022-07-06 13:57:26 -0300

    testando dados sensiveis

--- cypress/e2e/ALURAPIC/api-alurapic.cy.js
+++ cypress/e2e/ALURAPIC/api-alurapic.cy.js
@@ -1,5 +1,7 @@
+
 describe('busca fotos e dados' , () => {
 
+    //test de API REST
     it('buscar fotos do flavio', ()=> {
 
         cy.request({
@@ -11,6 +13,25 @@ describe('busca fotos e dados' , () => {
             expect(resposta.body).is.not.empty
             expect(resposta.body[0]).to.have.property('description')
             expect(resposta.body[0].description).to.be.equal('Farol iluminado')
+
+        })
+    })
+
+    //teste com dados sensiveis 
+    it.only('fazer login do flavio', ()=> {
+
+        cy.request({
+            method: 'POST',
+            url: 'https://apialurapic.herokuapp.com/user/login',
+            body: Cypress.env()
+
+        }).then((resposta) =>{
+            expect(resposta.status).to.be.equal(200)
+            expect(resposta.body).is.not.empty
+            expect(resposta.body).to.have.property('id')
+            expect(resposta.body.id).to.be.equal(1)
+            expect(resposta.body).to.have.property('email')
+            expect(resposta.body.email).to.be.equal("flavio@alurapic.com.br")
 
         })
     })
